18858de6cc583166e82b09df7d81923d52b0d588
Kaveen Abeywansa <kaveenabeywansa@gmail.com>
2018-10-04 22:47:40 +0530

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -34,7 +34,7 @@ export class MyApp {
       // Here you can do any higher level native things you might need.
       this.statusBar.styleDefault();
       this.splashScreen.hide();
-      localStorage.setItem('backendip','192.168.1.100');
+      localStorage.setItem('backendip','192.168.1.36');
     });
   }
 
